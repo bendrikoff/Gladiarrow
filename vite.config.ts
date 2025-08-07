543a4a51554d1b757083cc43b67b5f7e4da57888
Aleksandr.Bendrikov <bendrikoff0220@gmail.com>
2025-08-07 23:40:32 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/assets/*', // 👈 без dist/, копируем содержимое public/assets
-          dest: 'assets'          // 👈 попадёт в dist/assets
-        }
-      ]
-    })
-  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true
